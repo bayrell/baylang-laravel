c98a8ec52fc6e4d8590a6b81459e23674550180b
Ildar Bikmamatov <ildar@bayrell.org>
2025-12-04 20:09:29 +0500

    add files

--- README.md
+++ README.md
@@ -1,2 +1,225 @@
 # BayLang for Laravel
 
+Install BayLang Compiler
+```
+cd ~
+composer global require baylang/compiler
+```
+
+Install BayLang laravel:
+```
+cd laravel
+composer require baylang/laravel
+```
+
+Create project.json
+```
+{
+    "name": "Laravel project",
+    "description": "Description",
+    "license": "MIT",
+    "author": "",
+    "languages": ["php", "es6"],
+    "modules": [
+        {
+            "src": "./app",
+            "type": "lib"
+        }
+    ],
+    "assets": [
+        {
+            "type": "js",
+            "dest": "public/assets/app.js",
+            "modules": [
+                "App"
+            ]
+        }
+    ],
+    "exclude": []
+}
+```
+
+Create app/module.json
+```
+{
+	"name": "App",
+	"assets": [
+		"Components/Blocks/CSS.bay",
+		"Components/Pages/IndexPage/IndexPage.bay",
+		"Components/Pages/IndexPage/IndexPageModel.bay",
+		"ModuleDescription.bay"
+	],
+	"src": "./",
+	"dest": {
+		"php": "../resources/php",
+		"es6": "../resources/es6"
+	},
+	"allow": [
+		"\\.bay$"
+	]
+}
+```
+
+Add to composer.json path to find app components:
+```
+"App\\": ["app/", "resources/php"],
+```
+
+Create app/Components/Blocks/CSS.bay:
+```
+<class name="App.Components.Blocks.CSS">
+
+<style global="true">
+html, body{
+    padding: 0;
+    margin: 0;
+}
+</style>
+
+</class>
+```
+
+Create app/Components/Pages/IndexPage/IndexPage.bay:
+```
+<class name="App.Components.Pages.IndexPage.IndexPage">
+
+<use name="Runtime.Widget.Button" component="true" />
+
+<style>
+.index_page{
+	text-align: center;
+	padding-top: 100px;
+}
+</style>
+
+<template>
+	<div class="index_page">
+		<div>Hello {{ this.model.username }}!</div>
+		<Button @event:click="this.onClick()">Click</Button>
+	</div>
+</template>
+
+<script>
+
+void onClick()
+{
+	this.model.setUserName(this.model.username ~ "!");
+}
+
+</script>
+
+</class>
+```
+
+Create app/Components/Pages/IndexPage/IndexPageModel.bay:
+```
+namespace App.Components.Pages.IndexPage;
+
+use Runtime.BaseModel;
+use App.Components.Pages.IndexPage.IndexPage;
+
+
+class IndexPageModel extends BaseModel
+{
+	string component = classof IndexPage;
+	string username = "User";
+	
+	
+	/**
+	 * Set user name
+	 */
+	void setUserName(string value)
+	{
+		this.username = value;
+	}
+}
+```
+
+Create app/ModuleDescription.bay
+```
+namespace App;
+
+use Runtime.Entity.Hook;
+use Runtime.Web.Hooks.Components;
+use Runtime.Web.Hooks.SetupLayout;
+
+
+class ModuleDescription
+{
+	/**
+	 * Returns module name
+	 * @return string
+	 */
+	pure string getModuleName() => "App";
+	
+	
+	/**
+	 * Returns module name
+	 * @return string
+	 */
+	pure string getModuleVersion() => "0.0.1";
+	
+	
+	/**
+	 * Returns required modules
+	 * @return Dict<string>
+	 */
+	pure Dict<string> requiredModules() =>
+	{
+		"Runtime.Web": "*",
+		"Runtime.Widget": "*",
+	};
+	
+	
+	/**
+	 * Returns enities
+	 */
+	pure Collection<Dict> entities() =>
+	[
+		Components::hook([
+			"App.Components.Blocks.CSS",
+		]),
+		SetupLayout::hook({
+			"default": "Runtime.BaseLayout",
+		}),
+	];
+}
+```
+
+Add main route in file routes/web.php
+```
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Runtime\RenderContainer;
+
+/* Main page */
+Route::get('/', function () {
+    
+    /* Get render container */
+    $container = app(RenderContainer::class);
+    
+    /* Setup page */
+    $page = $container->layout->setPageModel("App.Components.Pages.IndexPage.IndexPageModel");
+    $page->username = "User";
+    
+    /* Render app */
+    return $container->renderApp();
+});
+```
+
+Download vue:
+```
+cd public/assets/core
+wget https://unpkg.com/vue@3/dist/vue.runtime.global.prod.js
+```
+
+Publish assets:
+```
+php artisan vendor:publish
+```
+
+Compile project:
+```
+baylang-php make_all
+```
---
+++ src/ServiceProvider.php
@@ -0,0 +1,174 @@
+<?php
+
+/*!
+ *  BayLang Technology
+ *
+ *  (c) Copyright 2016-2025 "Ildar Bikmamatov" <support@bayrell.org>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace Runtime\Laravel;
+
+use Runtime\Context;
+use Runtime\RenderContainer;
+use Runtime\Map;
+use Runtime\Method;
+use Runtime\Vector;
+use Runtime\VirtualDom;
+use Runtime\Entity\Provider;
+use Runtime\Hooks\RuntimeHook;
+use Runtime\Web\Hooks\AppHook;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+
+class ServiceProvider extends BaseServiceProvider
+{
+	/**
+	 * Register services
+	 */
+	public function register()
+	{
+		/* Global context */
+		$this->app->singleton(Context::class, function ($app){
+			
+			/* Get params */
+			$params = new Map([
+				"modules" => new Vector(
+					"App", "Runtime.Web", "Runtime.Widget"
+				),
+			]);
+			$params = Context::initParams($params);
+			
+			/* Create context */
+			$context = new Context($params);
+			\Runtime\rtl::setContext($context);
+			
+			/* Init context */
+			$context->init($params);
+			
+			/* Returns context */
+			return $context;
+		});
+		
+		/* Render container */
+		$this->app->singleton(RenderContainer::class, function ($app){
+			$container = new \Runtime\Web\RenderContainer();
+			$context = $app->get(Context::class);
+			$context->hook(RuntimeHook::CREATE_CONTAINER, new Map([
+				"container" => $container,
+			]));
+			return $container;
+		});
+	}
+	
+	
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Event::listen(RouteMatched::class, [$this, "onRouteMatched"]);
+		Event::listen(RequestHandled::class, [$this, "onRequest"]);
+		
+		/* Get context */
+		$context = $this->app->get(Context::class);
+		$hook = $context->provider("hook");
+		
+		/* Add hook */
+		$hook->register(RuntimeHook::LAYOUT_FOOTER, new Method($this, "onFooter"));
+		
+		/* Start context */
+		$context->start();
+		
+		/* Register assets */
+		$this->registerPublishes();
+	}
+	
+	
+	/**
+	 * Register assets
+	 */
+	public function registerPublishes()
+	{
+		$class = new \ReflectionClass(\Runtime\ModuleDescription::class);
+		$packagePath = $class->getFileName();
+		$packagePath = dirname($packagePath);
+		$packageAssetsPath = realpath($packagePath . '/../../assets');
+		
+		if (!$packageAssetsPath) return;
+		$this->publishes([$packageAssetsPath => public_path("assets/core")], "runtime-core");
+	}
+	
+	
+	/**
+	 * On request
+	 */
+	public function onRequest($event)
+	{
+		$container = $this->app->get(RenderContainer::class);
+		$container->request = $event->request;
+	}
+	
+	
+	/**
+	 * On route matched
+	 */
+	public function onRouteMatched($event)
+	{
+		$container = $this->app->get(RenderContainer::class);
+		$container->route = new \Runtime\Web\RouteModel(new Map([
+			"uri" => $event->route->uri(),
+			"name" => $event->route->getName(),
+		]));
+		$container->createlayout("default");
+		
+		$context = $this->app->get(Context::class);
+		$context->hook(AppHook::ROUTE_BEFORE, new Map([
+			"container" => $container,
+		]));
+	}
+	
+	
+	/**
+	 * On footer
+	 */
+	public function onFooter($params)
+	{
+		$components = $params->get("components");
+		
+		$version = \Runtime\ModuleDescription::getModuleVersion();
+		$assets = new Vector(
+			asset("assets/core/vue.runtime.global.prod.js?_=" . $version),
+			asset("assets/core/runtime.js?_=" . $version),
+			asset("assets/app.js"),
+		);
+		$items = $assets->map(function($path){
+			$vdom = new VirtualDom();
+			$vdom->name = "script";
+			$vdom->attrs = new Map([
+				"src" => $path,
+			]);
+			return $vdom;
+		});
+		
+		$components->appendItems($items);
+	}
+}
